dca3d546aa2cabac8bbd36ff1847d3aedb0c3e24
Rica Nguyen <22810211@student.hcmus.edu.vn>
2024-01-10 11:32:11 +0700

    update DFS

--- Lab1BasicSearch/BFS.py
+++ Lab1BasicSearch/BFS.py
@@ -1,11 +1,11 @@
-# graph = { 
-# 'A' : ['B','C'], 
-# 'B' : ['D', 'E'], 
-# 'C' : ['F'], 
-# 'D' : [], 
-# 'E' : ['F'], 
-# 'F' : [] 
-# } 
+# graph = {
+# 'A' : ['B','C'],
+# 'B' : ['D', 'E'],
+# 'C' : ['F'],
+# 'D' : [],
+# 'E' : ['F'],
+# 'F' : []
+# }
 # visited =[]
 # queue =[]
 # def BFS(visited,graph,start, end):
@@ -19,22 +19,22 @@
 #         for neighbor in graph[s]:
 #             if neighbor not in visited:
 #                 queue.append(neighbor)
-    
+
 # BFS(visited,graph,'A','E')
 #########################################
-graph = { 
-        '1': ['2', '3', '4'], 
-        '2': ['5', '6'], 
-        '5': ['9', '10'], 
-        '4': ['7', '8'], 
-        '7': ['11', '12'] 
-        } 
+graph = {
+    '1': ['2', '3', '4'],
+    '2': ['5', '6'],
+    '5': ['9', '10'],
+    '4': ['7', '8'],
+    '7': ['11', '12']
+}
 graph1 = {
     's': ['d', 'e', 'p'],
     'a': [],
     'b': ['a'],
     'c': ['a'],
-    'd': ['b','c','e'],
+    'd': ['b', 'c', 'e'],
     'e': ['h', 'r'],
     'f': ['c', 'g'],
     'g': [],
@@ -43,49 +43,52 @@ graph1 = {
     'q': [],
     'r': ['f']
 }
-graph2={
-    'a':['b', 'c'], 
-    'b':['a','d'],
-    'c':['a','k'],
-    'd':['b','e','m'],
-    'e':['d','m','n'],
-    'f':['s','p'],
-    'g':['m','t'],
-    'h':['s','k'],
-    'k':['c','h'],
-    'm':['d','n','g'],
-    'n':['e','m'],
-    'p':['f','q'],
-    'q':['p','r'],
-    'r':['q','t'],
-    't':['r','g'],
-    's':['h','f']
+graph2 = {
+    'a': ['b', 'c'],
+    'b': ['a', 'd'],
+    'c': ['a', 'k'],
+    'd': ['b', 'e', 'm'],
+    'e': ['d', 'm', 'n'],
+    'f': ['s', 'p'],
+    'g': ['m', 't'],
+    'h': ['s', 'k'],
+    'k': ['c', 'h'],
+    'm': ['d', 'n', 'g'],
+    'n': ['e', 'm'],
+    'p': ['f', 'q'],
+    'q': ['p', 'r'],
+    'r': ['q', 't'],
+    't': ['r', 'g'],
+    's': ['h', 'f']
 }
-graph3={
-    'a':['b', 'c'],
-    'b':['a','c','d'],
-    'c':['a','b','d'],
-    'd':['b','c','e','f','g'],
-    'e':['d','g'],
-    'f':['d','g'],
-    'g':['d','e','f']
+graph3 = {
+    'a': ['b', 'c'],
+    'b': ['a', 'c', 'd'],
+    'c': ['a', 'b', 'd'],
+    'd': ['b', 'c', 'e', 'f', 'g'],
+    'e': ['d', 'g'],
+    'f': ['d', 'g'],
+    'g': ['d', 'e', 'f']
 }
+
+
 def bfs(graph, start, end):
-    visited =[]
-    queue= []
+    visited = []
+    queue = []
     queue.append([start])
     while queue:
-        path=queue.pop()
-        node =path[-1] 
-        if node==end:
+        path = queue.pop(0)
+        node = path[-1]
+        if node == end:
             return path
         visited.append(node)
-        for neighbour in graph.get(node, []): 
-            if neighbour not in visited: 
-                new_path = list(path) 
-                new_path.append(neighbour) 
+        for neighbour in graph.get(node, []):
+            if neighbour not in visited:
+                new_path = list(path)
+                new_path.append(neighbour)
                 queue.append(new_path)
 
-print (bfs(graph, '1', '11')) 
-print (bfs(graph2, 'a', 'g'))
-print (bfs(graph3, 'a', 'g'))
+
+print(bfs(graph, '1', '11'))
+print(bfs(graph2, 'a', 'g'))
+print(bfs(graph3, 'a', 'g'))
--- Lab1BasicSearch/DFS.py
+++ Lab1BasicSearch/DFS.py
@@ -1,34 +1,102 @@
-graph = { 
-'A' : ['B','C'], 
-'B' : ['D', 'E'], 
-'C' : ['F'], 
-'D' : [], 
-'E' : ['F'], 
-'F' : [] 
-} 
-def DFS( graph,start, end):
-   
-    # # them start vao mang path
-    # path.append(start)
-    # # noi mang path va mang neighbor cua start 
-    # path.extend( graph[start])
-    # # neu path khac rong va top(path) khong la dich
-    # while path & path.index()!=end:
-    #     if len(graph[path.index()])==0:
-    #         path.pop(0)
-    #     else:
-    #         for member in len(graph[path.index()]):
-    #             path.pop(member)
-    #     return "complete"
-    # khoi tao 
-    path=[]
-    stack=[]
-    path.append(start) 
-    # them start vao path, them neighbor cua start vao stack
-    stack.append(graph[start])
-    # neu khong con neighor quay lại vi tri gan nhat cua stack tiep tuc lay neighbor khac
-    while path & path.index()!=end:
-        path.append(stack.pop())
-        
 
-DFS(graph ,'A','D')     
+graph1 = {
+    's': ['d', 'e', 'p'],
+    'a': [],
+    'b': ['a'],
+    'c': ['a'],
+    'd': ['b', 'c', 'e'],
+    'e': ['h', 'r'],
+    'f': ['c', 'g'],
+    'g': [],
+    'h': ['p', 'q'],
+    'p': ['q'],
+    'q': [],
+    'r': ['f']
+}
+graph2 = {
+    'a': ['b', 'c'],
+    'b': ['a', 'd'],
+    'c': ['a', 'k'],
+    'd': ['b', 'e', 'm'],
+    'e': ['d', 'm', 'n'],
+    'f': ['s', 'p'],
+    'g': ['m', 't'],
+    'h': ['s', 'k'],
+    'k': ['c', 'h'],
+    'm': ['d', 'n', 'g'],
+    'n': ['e', 'm'],
+    'p': ['f', 'q'],
+    'q': ['p', 'r'],
+    'r': ['q', 't'],
+    't': ['r', 'g'],
+    's': ['h', 'f']
+}
+graph3 = {
+    'a': ['b', 'c'],
+    'b': ['a', 'c', 'd'],
+    'c': ['a', 'b', 'd'],
+    'd': ['b', 'c', 'e', 'f', 'g'],
+    'e': ['d', 'g'],
+    'f': ['d', 'g'],
+    'g': ['d', 'e', 'f']
+}
+
+
+# DFS with stack
+def DFS(graph, start, end):
+    print(f'DFS start {start} to {end}:')
+    visited = []
+    stack = []
+    stack.append([start])
+
+    while stack:
+        path = stack.pop()
+
+        node = path[-1]
+        if node == end:
+            return path
+        visited.append(node)
+        for neighbor in graph.get(node, []):
+            if neighbor not in visited:
+                new_path = list(path)
+                new_path.append(neighbor)
+                stack.append(new_path)
+
+
+print(DFS(graph1, 's', 'g'))
+print(DFS(graph2, 's', 'g'))
+print(DFS(graph2, 'a', 'g'))
+
+
+# DFS with stack, and strategy deep node
+def DFSWithStrategy(graph, start, end):
+    print(f'DFS start {start} to {end} - with strategy deep node:')
+    visited = []
+    stack = []
+    stack.append([start])
+
+    def getIndexMaxNodeNeighbor(stack):
+        maxNodeNeighbor = []
+        for stackItem in stack:
+            if len(stackItem) > len(maxNodeNeighbor):
+                maxNodeNeighbor = stackItem
+        return stack.index(maxNodeNeighbor)
+
+    while stack:
+        index = getIndexMaxNodeNeighbor(stack)
+        path = stack.pop(index)
+
+        node = path[-1]
+        if node == end:
+            return path
+        visited.append(node)
+        for neighbor in graph.get(node, []):
+            if neighbor not in visited:
+                new_path = list(path)
+                new_path.append(neighbor)
+                stack.append(new_path)
+
+
+print(DFSWithStrategy(graph1, 's', 'g'))
+print(DFSWithStrategy(graph2, 's', 'g'))
+print(DFSWithStrategy(graph3, 'a', 'g'))
